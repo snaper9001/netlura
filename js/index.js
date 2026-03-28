8e9042a729bf8d234462bf28b761f52d786a3ae4
Luis M <luismquirinom@gmail.com>
2026-03-27 21:29:33 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,22 @@
+
+
+// Adicionar evento de clique aos perfis
+document.addEventListener('DOMContentLoaded', () => {
+    const perfis = document.querySelectorAll('.perfil');
+    
+    perfis.forEach(perfil => {
+        perfil.addEventListener('click', (event) => {
+            // Obter o nome do perfil
+            const nomePerfil = perfil.querySelector('p').textContent;
+            
+            // Obter a imagem do perfil
+            const imagemPerfil = perfil.querySelector('img').src;
+            
+            // Armazenar no localStorage
+            localStorage.setItem('perfilAtivoNome', nomePerfil);
+            localStorage.setItem('perfilAtivoImagem', imagemPerfil);
+            
+            console.log(`Perfil salvo: ${nomePerfil} - ${imagemPerfil}`);
+        });
+    });
+});
